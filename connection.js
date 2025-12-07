562a6afaa6c362b40213a96789b4d0023b321b24
Ajith C Ravi <aravi@presidio.com>
2025-12-07 15:46:02 +0530

    Update the connection code
    (cherry picked from commit f685610e40e72ba09ff36f5eb5a02e21bfd54d20)

--- connection.js
+++ connection.js
@@ -1,6 +1,11 @@
+// A connection consists of three things
+// 1. What is the source of the connection
+// 2. What is the target of the connection
+// 3. How important is this connection to the target
+
 export default class Connection {
-  constructor({ source, target, importance }) {
-    this.source = source;
+  constructor({ sourceName, target, importance }) {
+    this.sourceName = sourceName;
     this.target = target;
     this.importance = importance;
   }
